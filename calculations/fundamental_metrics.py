26f2117ad9c817cbd27598832cdaa9fd18a2f5ec
Hunter Johnson <hhjohnso@indiana.edu>
2016-07-23 20:40:25 -0400

    Update fundamental_metrics.py

--- calculations/fundamental_metrics.py
+++ calculations/fundamental_metrics.py
@@ -1,8 +1,134 @@
-def get_ebitda(symbol):
+def ebitda(symbol):
   return None 
 
-def get_pe_ratio(symbol):  #price to earnings ratio
+def pe_ratio(symbol):  #price to earnings ratio
   return None
   
-def get_debt_to_equity_ratio(symbol):
+def debt_to_equity_ratio(symbol):
+  return None
+
+
+#Example data comes from the income statement, balance sheet and cash flow statement found in the Financial Statement Analysis columns in the March, May and July 2012 issues of the AAII Journal, which are linked here.	 
+ 	 
+#Dollar amounts are in millions of dollars.	 
+ 	 
+#### Activity Ratios	 
+"""Inventory turnover	=	cost of goods sold ÷ average inventory	 =	$500 ÷ $190 = 2.6x	 """
+
+def inventory_turnover_ratio():
+  return None 
+  
+"""Receivables turnover	=	net revenue ÷ average receivables	 
+=	$1,000 ÷ $128.5 = 7.8x	 """
+
+def receivables_turnover_ratio():
+  return None
+
+"""Payables turnover	=	purchases* ÷ average payables	 
+=	$520 ÷ $90 = 5.8x	 """"
+
+def payables_turnover_ratio():
+  return None
+  
+"""Asset turnover	=	net revenues ÷ average total assets	 
+=	$1,000 ÷ $1,391 = 0.72x	 """
+
+def asset_turnover_ratio():
+  return None
+
+#Liquidity Ratios	 
+"""Current ratio	=	current assets ÷ current liabilities	 
+=	$685 ÷ $750 = 0.91x	"""
+
+def current_ratio():
+  return None
+
+"""Quick ratio	=	(cash + short-term marketable securities + accounts receivable) ÷ current liabilities	 
+=	$340 ÷ $750 = 0.45x	 """
+
+def quick_ratio():
+  return None
+
+"""Cash ratio	=	(cash + short-term marketable securities) ÷ current liabilities	 
+=	$200 ÷ $750 = 0.27x	""" 
+
+def cash_ratio():
+  return None
+ 	 
+#Solvency Ratios	 
+"""Debt-to-assets ratio	=	total liabilities ÷ total assets	 
+=	$1,067 ÷ $1,485 = 0.72, or 72%	""" 
+
+def debt_to_assets_ratio():
+  return None
+
+"""Debt-to-capital ratio	=	total debt* ÷ (total debt* + total shareholder’s equity)	 
+=	$517 ÷ $935 = 0.55, or 55%	 """
+
+def debt_to_capital_ratio():
+  return None
+  
+"""Debt-to-equity ratio	=	total debt* ÷ total shareholder’s equity	 
+=	$517 ÷ $418 = 1.24, or 124%	 """
+
+def debt_to_equity_ratio():
+  return None
+
+"""Interest coverage ratio	=	earnings before interest and taxes* ÷ interest payments	 
+=	$230 ÷ $100 = 2.3x	""" 
+
+def interest_coverage_ratio():
+  return None
+ 	 
+#Profitability Ratios	 
+"""Gross profit margin	=	gross income ÷ net revenue	 
+=	$500 ÷$1,000 = 0.5, or 50%	 """
+
+def gross_profit_margin():
+  return None
+
+"""Operating profit margin	=	operating income ÷ net revenue	 
+=	$180 ÷ $1,000 = 0.18, or 18%	 """
+
+def operating_profit_margin():
+  return None
+
+"""Net profit margin	=	net income ÷ net revenue	 
+=	$82.75 ÷ $1,000 = 0.083, or 8.3%	 """
+
+def net_profit_margin():
+  return None
+
+"""Return on assets (ROA)	=	net income ÷ total assets	 
+=	$82.75 ÷ $1,485 = 0.056, or 5.6%	 """
+
+def return_on_assets():
+  return None
+
+"""Return on equity (ROE)	=	net income ÷ total stockholder’s equity	 =	$82.75 ÷ $418 = 0.20, 20"""
+
+def return_on_equity():
+  return None
+  
+ 
+#calculated terms:	 
+"""purchases	= 	cost of goods sold + ending inventory – beginning inventory	 
+= 	$500 + $200 – $180 = $520	 """
+
+def purchases():
+  return None
+
+"""total debt	= 	notes payable + current portion of long-term debt + long-term debt	 
+= 	$100 + $150 + $267 = $517	 """
+
+def total_debt():
+  return None
+
+"""earnings before interest and taxes	= 	net income + income taxes + interest expense	 
+= 	$82.75 + $47.25 + $100 = $230	 """
+
+def ebit():
+  return None
+  
+def ebitda():
   return None
