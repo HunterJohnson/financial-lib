3438196451fa2524c685edc79c6d0e54cdaaa9bd
Hunter Johnson <hhjohnso@indiana.edu>
2016-07-23 20:57:58 -0400

    Update accounting.py

--- calculations/accounting.py
+++ calculations/accounting.py
@@ -1,1 +1,13 @@
 # A = L + OE
+
+"""
+Owners equity = Contributed Capital + Retained Earnings
+Retained Earnings = Net Income − Dividends
+
+Net Income = Income − Expenses
+"""
+
+class Account:
+  
+class Transaction:
+
