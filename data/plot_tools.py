e778704ba2a49f33bdef2e0e3064d76bf6ff17a0
Hunter Johnson <hhjohnso@indiana.edu>
2016-07-25 12:45:55 -0400

    Update plot_tools.py

--- data/plot_tools.py
+++ data/plot_tools.py
@@ -1,1 +1,10 @@
 # plotting security prices including fundamental metrics & ratios
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import Quandl
+
+class finPlot(self, ticker):
+  self.ticker = ticker
+
